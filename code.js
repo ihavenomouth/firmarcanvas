2d32edf6a634db8138e72d383dc4f2bc7c91e1c4
Javier <javmanfer@gmail.com>
2026-02-10 12:49:34 +0100

    primera subida

---
+++ code.js
@@ -0,0 +1,41 @@
+"use strict";
+
+import {firma,borrarFirma} from "./firma.js";
+
+const canvasFirma = document.querySelector("#canvasFirma");
+
+// Gestionamos el canvas para que se pueda firmar
+firma(canvasFirma);
+
+
+
+// Botón para borrar la firma
+document.querySelector("#btnBorrar").addEventListener("click", e=>{
+  borrarFirma();
+});
+
+
+
+
+// Botón para procesar la firma
+document.querySelector("#btnFirmar").addEventListener("click", e=>{
+  
+  const datosEnBase64ParaEnviarAlServidor = document.querySelector("#datosEnBase64ParaEnviarAlServidor");
+
+  //TODO: Importante - El usar toDataUrl() es cómodo, pero hace que ocupe un 33% más
+  // la imagen resultante. Normalmente esto no será un problema, pero podría serlo con imágenes grandes.
+  // Otra manera de hacerlo sería usar el método toBlob(), que genera los datos en binario.
+
+  // datos de la firma: imagen en base64
+  const datosBase64 = canvasFirma.toDataURL("image/png");
+  
+  // Se muestran los datos que se mandarían al servidor en la web
+  datosEnBase64ParaEnviarAlServidor.textContent = datosBase64;
+
+  // Se muestra la imagen que se crea con los datos que devolvería el servidor
+  imagenRecuperadaDelServidor.src = datosBase64;
+});
+
+
+
+
---
+++ firma.js
@@ -0,0 +1,155 @@
+
+/**
+ * Función auxiliar que sirve para saber la posición dentro del canvas en la que se produjo un evento
+ * de ratón (mouseenter, mouseleave, mouseup...)
+ * @param {MouseEvent} e 
+ * @returns Object - Objeto {x,y} con la posición dentro del canvas 
+ */
+const calcularPosiciónDelEventoEnElCanvas = (e,canvas) => {
+  // Posición del canvas dentro de la web
+  const posiciónCanvas = {
+    x: canvas.getBoundingClientRect().x,
+    y: canvas.getBoundingClientRect().y
+  };
+
+  //Posición  del canvas donde se ha producido el evento:
+  return {
+    x: e.clientX - posiciónCanvas.x,
+    y: e.clientY - posiciónCanvas.y
+  }
+
+}
+
+
+
+const característicasDeLaLínea = (ctx) =>{
+  // Características de la línea
+  ctx.lineWidth = 2;
+  ctx.lineJoin = "round";
+  ctx.lineCap = "round";
+}
+
+
+
+
+/**
+ * Gestiona un canva para que se pueda firmar dentro de él
+ * @param {HTMLCanvasElement} canvas 
+ */
+const firma = (canvas) => {
+
+  const ctx = canvas.getContext("2d");
+
+  //Establecemos las características de la línea
+  característicasDeLaLínea(ctx);
+
+  // variable que indica si se está dibujando (el botón está pulsado) o simplemente el ratón
+  // ha pasado por encima del canvas
+  let dibujando = false;
+
+
+
+  canvas.addEventListener("mousedown", e => {
+    //Posición dentro del canvas donde se ha hecho click:
+    const puntoInicio = calcularPosiciónDelEventoEnElCanvas(e,canvas);
+
+    //Movemos el bolígrafo a la posición inicial y marcamos que se está dibujando
+    ctx.moveTo(puntoInicio.x, puntoInicio.y);
+    dibujando = true;
+  });
+
+
+
+
+
+  canvas.addEventListener("mousemove", e => {
+    //Posición dentro del canvas donde se ha movido:
+    const puntoFinal = calcularPosiciónDelEventoEnElCanvas(e,canvas);
+
+    //Dibujamos una línea desde el punto al que se estaba hasta la posición actual
+    if (dibujando) {
+      ctx.lineTo(puntoFinal.x, puntoFinal.y);
+      ctx.stroke();
+    }
+  });
+
+
+
+
+  canvas.addEventListener("mouseup", e => {
+    //si dejamos de pulsar el botón se dibuja lo que haya pendiente y se
+    //marca que ya no estamos dibujando 
+    ctx.stroke(); // Render the path
+    dibujando = false;
+  });
+
+
+
+  canvas.addEventListener("mouseleave", e => {
+    //si salimos del canvas se dibuja lo que haya pendiente y se
+    //marca que ya no estamos dibujando 
+    ctx.stroke(); // Render the path
+    dibujando = false;
+  });
+
+
+
+
+
+
+
+  canvas.addEventListener("touchstart", e => {
+    e.preventDefault();
+    // console.log( e.touches[0] );
+
+    //Posición dentro del canvas donde se ha hecho click:
+    const puntoInicio = calcularPosiciónDelEventoEnElCanvas(e.touches[0],canvas);
+
+    //Movemos el bolígrafo a la posición inicial y marcamos que se está dibujando
+    ctx.moveTo(puntoInicio.x, puntoInicio.y);
+    dibujando = true;
+  });
+
+
+  canvas.addEventListener("touchmove", e => {
+    e.preventDefault();
+
+    //Posición dentro del canvas donde se ha movido:
+    const puntoFinal = calcularPosiciónDelEventoEnElCanvas(e.touches[0],canvas);
+
+    //Dibujamos una línea desde el punto al que se estaba hasta la posición actual
+    if (dibujando) {
+      ctx.lineTo(puntoFinal.x, puntoFinal.y);
+      ctx.stroke();
+    }
+  });
+
+
+
+
+  canvas.addEventListener("touchend", e => {
+    //si dejamos de pulsar el botón se dibuja lo que haya pendiente y se
+    //marca que ya no estamos dibujando 
+    ctx.stroke(); // Render the path
+    dibujando = false;
+  });
+
+}
+
+
+
+const borrarFirma=(canvas)=>{
+  const ctx = canvasFirma.getContext("2d");
+  ctx.reset();
+  //Reestablecemos las características de la línea
+  característicasDeLaLínea(ctx);
+}
+
+
+
+
+
+export {firma, borrarFirma};
+
+
+
